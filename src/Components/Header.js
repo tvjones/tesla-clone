f13ef50b99602c59c2696b6bb28f403a4a067146
tvjones <43976584+tvjones@users.noreply.github.com>
2022-07-14 23:26:03 -0500

    fixed nav bar dimensions

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,13 +11,11 @@ function Header() {
 
 const toggleMenu = () =>{
     (document.getElementById('menu').className === 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left'?document.getElementById('menu').className ='w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left': document.getElementById('menu').className = 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left')
-
-
 }
 
   return (
-    <div className='header w-full grid grid-cols-3' >
-        <img className='header-logo col-start-1 pl-8' src='./images/tesla-logo.png' width='20%' height='100%' alt='logo' /> 
+    <div className='header w-full grid grid-cols-3 box-border' >
+        <img className='header-logo pl-[10%]' src='./images/tesla-logo.png' width='20%' height='100%' alt='logo' /> 
         <nav className='product-links'>
             <ul className='grid grid-cols-6'>
                 <li className='hover:rounded-full hover:bg-gray-400 cursor-pointer'> Model 3</li>
@@ -28,11 +26,11 @@ const toggleMenu = () =>{
                 <li className='hover:rounded-full hover:bg-gray-400 cursor-pointer'> Solar Panels</li>
             </ul>
         </nav>
-        <nav className='utility-links pl-72'>
-            <ul className='grid grid-cols-3'>
-                <li className='hover:rounded-full hover:bg-gray-400 cursor-pointer'>Shop</li>
-                <li className='hover:rounded-full hover:bg-gray-400 cursor-pointer'>Account</li>
-                <li className='hover:rounded-full hover:bg-gray-400 cursor-pointer' onClick= { ()=>{  (document.getElementById('menu').className === 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left'?document.getElementById('menu').className ='w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left': document.getElementById('menu').className = 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left')} }>Menu</li>
+        <nav className='utility-links'>
+            <ul className='grid grid-cols-3 pl-[30%]'>
+                <li className='hover: w-[50%]  hover:rounded-full hover:bg-gray-400 cursor-pointer'>Shop</li>
+                <li className='hover: w-[50%]  hover:rounded-full hover:bg-gray-400 cursor-pointer'>Account</li>
+                <li className='hover: w-[50%] hover:rounded-full hover:bg-gray-400 cursor-pointer' onClick= { ()=>{  (document.getElementById('menu').className === 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left'?document.getElementById('menu').className ='w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left': document.getElementById('menu').className = 'hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left')} }>Menu</li>
             </ul>
         </nav>
         <div id = "menu" className="hidden w-[20rem] fixed inset-y-0 right-0  bg-white col-start-3 ml-[20rem] text-left"><Menu /></div>
